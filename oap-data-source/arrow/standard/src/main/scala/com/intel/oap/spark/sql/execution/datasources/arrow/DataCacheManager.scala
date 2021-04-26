ed69da722d9150d50505b7ee2e5c23db090d0514
liyan <hanily@msn.com>
2021-04-26 16:17:41 +0800

    test

--- oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
+++ oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
@@ -1,11 +1,17 @@
 package com.intel.oap.spark.sql.execution.datasources.arrow
 
 import java.util
+import java.util.UUID
 
 import org.apache.arrow.dataset.scanner.ScanTask
+import org.apache.arrow.memory.BaseAllocator
+import org.apache.arrow.vector.VectorSchemaRoot
 import org.apache.arrow.vector.dictionary.Dictionary
 import org.apache.arrow.vector.types.pojo.DictionaryEncoding
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.execution.datasources.v2.arrow.{SparkMemoryUtils, SparkMemoryUtils1}
+import org.apache.spark.sql.execution.datasources.v2.arrow.SparkMemoryUtils.{ExecutionMemoryAllocationListener, close, getLocalTaskContext, getTaskMemoryManager, softClose, taskToAllocatorMap}
+import org.apache.spark.util.TaskCompletionListener
 
 import scala.collection.mutable.HashMap
 
@@ -28,14 +34,28 @@ object DataCacheManager extends Logging {
     val newVectorData = vectorData.map {
       x =>
         val oldValueVectors = x.valueVectors
-        val newValueVectors = oldValueVectors.slice(0, oldValueVectors.getRowCount)
+
+        import org.apache.arrow.vector.VectorUnloader
+        val unloader = new VectorUnloader(oldValueVectors)
+        import org.apache.arrow.vector.ipc.message.ArrowRecordBatch
+        val recordBatch = unloader.getRecordBatch
+
+
+        val new_root =
+          VectorSchemaRoot.create(oldValueVectors.getSchema, SparkMemoryUtils1.arrowAllocator(file))
+        import org.apache.arrow.vector.VectorLoader
+        val loader = new VectorLoader(new_root)
+        loader.load(recordBatch)
+
+
+//        val newValueVectors = oldValueVectors.slice(0, oldValueVectors.getRowCount)
         val newDic = new util.HashMap[java.lang.Long, Dictionary]()
         x.dictionaryVectors.forEach {
           (k, v) =>
             newDic.put(k, new Dictionary(v.getVector,
               new DictionaryEncoding(v.getEncoding.getId, v.getEncoding.isOrdered, v.getEncoding.getIndexType)))
         }
-        new ScanTask.ArrowBundledVectors(newValueVectors, newDic)
+        new ScanTask.ArrowBundledVectors(new_root, newDic)
     }
     vData match {
       case Some(list) =>
