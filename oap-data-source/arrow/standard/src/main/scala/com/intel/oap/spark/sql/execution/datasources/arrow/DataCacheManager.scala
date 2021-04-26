f4b9f923366d6cd2dbfbbf63982b61d71ea65d0e
liyan <hanily@msn.com>
2021-04-26 14:40:25 +0800

    test

--- oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
+++ oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
@@ -1,6 +1,10 @@
 package com.intel.oap.spark.sql.execution.datasources.arrow
 
+import java.util
+
 import org.apache.arrow.dataset.scanner.ScanTask
+import org.apache.arrow.vector.dictionary.Dictionary
+import org.apache.arrow.vector.types.pojo.DictionaryEncoding
 import org.apache.spark.internal.Logging
 
 import scala.collection.mutable.HashMap
@@ -10,7 +14,7 @@ class DataCacheManager {
   val dataCacheManager = new HashMap[String, List[ScanTask.ArrowBundledVectors]]()
 }
 
-object DataCacheManager extends Logging{
+object DataCacheManager extends Logging {
   private val _data = new DataCacheManager
 
   def getVectorData(file: String): Option[List[ScanTask.ArrowBundledVectors]] = {
@@ -21,11 +25,23 @@ object DataCacheManager extends Logging{
   def saveVectorData(file: String, vectorData: List[ScanTask.ArrowBundledVectors]): Unit = {
     logError(s"-=-==-=-saveVectorData , file = ${file}")
     val vData = _data.dataCacheManager.get(file)
+    val newVectorData = vectorData.map {
+      x =>
+        val oldValueVectors = x.valueVectors
+        val newValueVectors = oldValueVectors.slice(0, oldValueVectors.getRowCount)
+        val newDic = new util.HashMap[java.lang.Long, Dictionary]()
+        x.dictionaryVectors.forEach {
+          (k, v) =>
+            newDic.put(k, new Dictionary(v.getVector,
+              new DictionaryEncoding(v.getEncoding.getId, v.getEncoding.isOrdered, v.getEncoding.getIndexType)))
+        }
+        new ScanTask.ArrowBundledVectors(newValueVectors, newDic)
+    }
     vData match {
       case Some(list) =>
-        _data.dataCacheManager.put(file, list ++ vectorData)
+        _data.dataCacheManager.put(file, list ++ newVectorData)
       case None =>
-        _data.dataCacheManager.put(file, vectorData)
+        _data.dataCacheManager.put(file, newVectorData)
     }
   }
 }
