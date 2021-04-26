14dda2879960d4975d5ef527c2bf9b756a0af2d3
liyan <hanily@msn.com>
2021-04-26 11:17:06 +0800

    test

--- oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
+++ oap-data-source/arrow/standard/src/main/scala/com/intel/oap/spark/sql/execution/datasources/arrow/DataCacheManager.scala
@@ -1,6 +1,7 @@
 package com.intel.oap.spark.sql.execution.datasources.arrow
 
 import org.apache.arrow.dataset.scanner.ScanTask
+import org.apache.spark.internal.Logging
 
 import scala.collection.mutable.HashMap
 
@@ -9,14 +10,16 @@ class DataCacheManager {
   val dataCacheManager = new HashMap[String, List[ScanTask.ArrowBundledVectors]]()
 }
 
-object DataCacheManager {
+object DataCacheManager extends Logging{
   private val _data = new DataCacheManager
 
   def getVectorData(file: String): Option[List[ScanTask.ArrowBundledVectors]] = {
+    logError(s"-=-==-=-getVectorData , file = ${file}")
     _data.dataCacheManager.get(file)
   }
 
   def saveVectorData(file: String, vectorData: List[ScanTask.ArrowBundledVectors]): Unit = {
+    logError(s"-=-==-=-saveVectorData , file = ${file}")
     val vData = _data.dataCacheManager.get(file)
     vData match {
       case Some(list) =>
